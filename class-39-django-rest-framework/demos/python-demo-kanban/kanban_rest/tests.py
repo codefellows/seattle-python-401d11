47491f1ef60c3c24fd63a25bd666085ec123cd9e
JB <jb.tellez@gmail.com>
2019-04-11 16:31:07 -0700

    adds API test

--- class-39-django-rest-framework/demos/python-demo-kanban/kanban_rest/tests.py
+++ class-39-django-rest-framework/demos/python-demo-kanban/kanban_rest/tests.py
@@ -1,4 +1,11 @@
 from django.test import TestCase, RequestFactory
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, force_authenticate, APIRequestFactory
+from board.models import Category, Card
+from django.contrib.auth.models import User
+from kanban_rest.views import CategoryListApiView
+import json
 
 
 class TestUserAPI(TestCase):
@@ -12,7 +19,6 @@ class TestUserAPI(TestCase):
         self.assertIn(b'"username":"test_user"', response.content)
 
     def test_user_login(self):
-        import json
         user = {
             'username': 'test_user',
             'email': 'user@user.com',
@@ -44,8 +50,34 @@ class TestUserAPI(TestCase):
         self.assertEqual(response.status_code, 200)
 
 
-class TestCategoryApi(TestCase):
-    pass
+class TestCategoryApi(APITestCase):
+
+    def test_get_categories(self):
+
+        ############# move into helper/factor when ready ###
+        user = {
+            'username': 'test_user',
+            'email': 'user@user.com',
+            'password': 'test_pw'
+        }
+        self.client.post('/api/v1/register', user)
+        response = self.client.post('/api/v1/login', user)
+        
+        user = User.objects.get(username='test_user')
+        #####################################################
+
+        view = CategoryListApiView.as_view()
+
+        # Make an authenticated request to the view...
+        factory = APIRequestFactory()
+        
+        request = factory.get('/api/v1/category/')
+        force_authenticate(request, user=user, token=user.auth_token)
+        response = view(request)
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(len(Category.objects.all()), 0)
 
 
 class TestCardApi(TestCase):
